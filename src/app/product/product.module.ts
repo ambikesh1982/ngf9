f091294379b0ae8c50676ccb1e0dfdda7b7e428e
ambikesh1982 <ambikesh1982@gmail.com>
2018-02-13 00:32:02 +0800

    added product routes

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
 import { FooditemListComponent } from './fooditem-list/fooditem-list.component';
 import { FooditemDetailComponent } from './fooditem-detail/fooditem-detail.component';
 import { FooditemNewComponent } from './fooditem-new/fooditem-new.component';
 import { FooditemModifyComponent } from './fooditem-modify/fooditem-modify.component';
 import { FooditemService } from './fooditem.service';
+import { MaterialModule } from '../material.module';
+
+const productRoutes: Routes = [
+  { path: '', redirectTo: '/list', pathMatch: 'full' },
+  { path: 'list', component: FooditemListComponent },
+  { path: 'detail/:id', component: FooditemDetailComponent },
+  { path: 'new', component: FooditemNewComponent },
+  { path: 'modify/:id', component: FooditemModifyComponent },
+  ];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    RouterModule.forChild(productRoutes)
   ],
-  declarations: [FooditemListComponent, FooditemDetailComponent, FooditemNewComponent, FooditemModifyComponent],
+  declarations: [
+    FooditemListComponent,
+    FooditemDetailComponent,
+    FooditemNewComponent,
+    FooditemModifyComponent
+  ],
   providers: [FooditemService]
 })
-export class ProductModule { }
+export class ProductModule {}
