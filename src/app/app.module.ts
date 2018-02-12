275df7bdf77292653367ce90801c0803c150c6c5
ambikesh1982 <ambikesh1982@gmail.com>
2018-02-12 22:43:14 +0800

    material2 setup done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
+import { MaterialModule } from './material.module';
+
 import { AppComponent } from './app.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/material.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSidenavModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatStepperModule,
+  MatToolbarModule,
+} from '@angular/material';
+
+@NgModule({
+  exports: [
+    // CommonModule
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSidenavModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatStepperModule,
+    MatToolbarModule,
+  ],
+  declarations: []
+})
+export class MaterialModule { }
